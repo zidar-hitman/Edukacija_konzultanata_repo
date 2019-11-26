6b2b38af8e2b13558ceb1227a7bf7f37246d89a4
mlatko <matko.radic@gmail.com>
2019-11-26 15:54:58 +0100

    drugi

--- student.sql
+++
@@ -1,5 +0,0 @@
-select
-punoime=ime+' '+prezime, adresa=a.broj+' '+a.ulicamNazivFakulterta=f.NazivFakulteta
-from omega.student s
-join omega.adresa a on a,id=s.adresaid
-left hoin omega.fakultet f on f.id=s.id
